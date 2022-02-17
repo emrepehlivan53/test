f53c83a45b894e3f703bd1011c5fc5ff45918118
YUNUS EMRE PEHLİVAN <emrepehlivan53@gmail.com>
2022-02-17 17:07:59 +0300

    readme

---
+++ test.ps1
@@ -0,0 +1,14 @@
+$Name = 'Yunus Emre'
+"Merhaba $Name"
+
+"Merhaba $Name. Bugun neler yapiyorsun"
+
+
+$ServerListStr = 'Server1,Server2,Server3,Server4'
+($ServerListStr -split ',')
+
+
+Get-Service -Name W3SVC 
+
+[System.String] | Get-Member -Static
+
